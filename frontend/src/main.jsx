574cab8750c9e15a2240de66e135f5dd211c5a48
Sneha Sah <snehasah22@ifheindia.org>
2025-10-29 19:26:37 +0530

    removed browser router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from "./components/ThemeContext.jsx";
-import { BrowserRouter } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+     <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
